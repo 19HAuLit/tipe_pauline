b146db6abe6401f2cda0de2a636f3261b8bdfbe0
19HAuLit <67673804+19HAuLit@users.noreply.github.com>
2024-03-21 18:22:55 +0100

    Add files via upload

--- Classification_mesure (2).py
+++ Classification_mesure (2).py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 
 
 #Import des données :
-data = pd.read_csv('acceleration_2023-11-09_17-57-14.csv',sep=';')
+data = pd.read_csv('donne _final_11.csv',sep=';')
 
 #Affichage du dataframe pandas :
-donnees = ['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13','a14','a15','a16','a17','a18','a19','a20','type_chute']
+donnees = ['a1','a2','a3','a4','a5','a6','a7','a9','a10','a11','a12','a13','a14','a15','a16','a17','a18','a19','a20','a21','a22','a23','a24','a25','a26','a27','type_chute']
 df=pd.DataFrame(data[donnees])
 print(df)
 
--- data.py
+++ data.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import os
 
+
+data = pd.read_csv('acceleration_2023-11-09_17-57-14.csv',sep=',')
+
+#Affichage du dataframe pandas :
+donnees = ['time','ax','ay','az','atotal']
+df=pd.DataFrame(data[donnees])
+
+ax_list = [element for element in df['ax']]
+
+nom_fichier = 'data_ax.csv'
+
+if not os.path.exists(nom_fichier):
+    with open(nom_fichier, 'w') as file:
+        for i in range(len(ax_list)):
+            file.write(f'a{i},')
+        file.write('type_chute\n')
+        print('Fichier créée')
+
+
+with open(nom_fichier, 'a') as file:
+    for ax in ax_list:
+        file.write(f'{ax},')
+    file.write('0\n')
+    print('Nouvelle données ajoutées')
